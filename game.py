79a1c9cedd13d07a3eee86631cc2510b337de72f
DaRealFreak <steffen.keuper@web.de>
2024-10-05 08:58:28 +0200

    stop trying to select the next level after completing level 7

--- game.py
+++ game.py
@@ -35,6 +35,7 @@ class Game:
 
         # Possible gear parts to transcend in order of the game (left to right and top to bottom)
         self.last_seen_gear_part = ''
+        self.last_seen_transcendence_level = 0
         self.possible_gear_parts = [
             'helmet',
             'shoulder',
@@ -70,8 +71,10 @@ class Game:
         """
         self._focus_lostark_window()
         self.last_information = self.calculator.get_current_information()
-        if self.last_information.gear_part and self.last_information.gear_part != self.last_seen_gear_part:
-            self.last_seen_gear_part = self.last_information.gear_part
+        if self.last_information.gear_part:
+            self.last_seen_transcendence_level = int(self.last_information.level.prediction)
+            if self.last_information.gear_part != self.last_seen_gear_part:
+                self.last_seen_gear_part = self.last_information.gear_part
 
         if self.last_information.flowers == 0:
             print('No flowers found on the screen, assume level got completed.')
@@ -117,8 +120,8 @@ class Game:
             print('Exiting, please check the game state.')
             return True
 
-        # Check if there are enough flowers to continue
-        if not self.has_flowers_to_continue():
+        # Check if there are enough flowers to continue and we aren't in a SelectNextLevel interaction
+        if not self.has_flowers_to_continue() and not isinstance(interaction, SelectNextLevel):
             print('Not enough flowers, resetting the level.')
             self.reset_level()
             return False
@@ -173,9 +176,13 @@ class Game:
             return False
         elif isinstance(interaction, SelectNextLevel):
             if self.auto_unlock_next_level:
-                print('Selecting next level if possible for gear part: ' + self.last_information.gear_part)
-                self.select_next_level()
-                return False
+                if self.last_seen_transcendence_level == 7:
+                    print('Level 7 reached, quitting transcendence.')
+                    return True
+                else:
+                    print('Selecting next level if possible for gear part: ' + self.last_seen_gear_part)
+                    self.select_next_level()
+                    return False
             else:
                 print('Auto unlock is disabled, quitting transcendence.')
                 return True
@@ -214,7 +221,7 @@ class Game:
         # wait for menu animation
         time.sleep(1)
 
-        # press okay button for soundstone fragments
+        # press okay button for Soundstone fragments
         self._click(x=958, y=674)
         time.sleep(0.25)
 
@@ -235,6 +242,10 @@ class Game:
 
         :return:
         """
+        # Retrieve the Soundstone fragments from the previous level
+        self._click(x=950, y=676)
+        time.sleep(0.25)
+
         # Confirm the previous level
         self._click(x=962, y=1026)
         # Click on arrow to navigate to the next level
